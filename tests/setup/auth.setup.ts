090e017a3bfccafd758ab8ba000879be207dfcee
Ugur <ugur.ayvaz@aracom.de>
2025-06-02 15:28:42 +0200

    test

--- tests/setup/auth.setup.ts
+++ tests/setup/auth.setup.ts
@@ -14,7 +14,7 @@ setup("Login and save storage state", async ({ page, context }) => {
 
   await header.languageSwitcher.selectLanguage("English");
   await header.accountManager.goToLogin();
-  await page.waitForTimeout(5000);
+  await page.waitForTimeout(10000);
   await header.accountManager.login.login(user.email, user.password);
   await page.screenshot({ path: "debug-after-login.png", fullPage: true });
 
